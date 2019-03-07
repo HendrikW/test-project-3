58cb5be1d726f858c0ab341acab420a7c78ea4c8
Hendrik Wermser <hendrik.wermser@ironhack.com>
2019-03-07 20:02:00 +0100

    makes BE/FE integration work for the first time

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios'
+
 class App extends Component {
+
+  state = {
+    have: 'LOADING'
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/').then((res) => {
+      this.setState({ have: res.data.have })
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            HAVE: {this.state.have} !
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
--- server/app.js
+++ server/app.js
@@ -49,6 +49,11 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+const cors = require('cors');
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000'] // <== this will be the URL of our React app (it will be running on port 3000)
+}));
 
 
 const index = require('./routes/index');
